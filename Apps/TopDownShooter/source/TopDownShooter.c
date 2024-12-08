e9b2ebd60762c79cf75b4e06067c68598112de3f
Pietro Romano <drum97@gmail.com>
2024-12-07 22:42:25 -0300

    Simple Physics2DSystem test

--- Apps/TopDownShooter/source/TopDownShooter.c
+++ Apps/TopDownShooter/source/TopDownShooter.c
@@ -18,12 +18,45 @@ typedef struct Transform2DComponent
     float y;
 } Transform2DComponent;
 
+typedef struct Physics2DComponent
+{
+    float velocity_x;
+    float velocity_y;
+} Physics2DComponent;
+
 ECS_ComponentType transform2d_component_type;
+ECS_ComponentType physics2d_component_type;
 
 typedef struct Render2DSystem
 {
     PlatformRenderer platform_renderer;
 } Render2DSystem;
+
+typedef struct Physics2DSystem
+{
+
+} Physics2DSystem;
+
+void Physics2DSystem_Init()
+{
+
+}
+
+void Physics2DSystem_Update(ECS_Registry* registry, ECS_System* self)
+{
+    for (uint64_t i = 0; i < self->entities->element_count; i++)
+    {
+        ECS_Entity* entity = ECS_Vector_Get(self->entities, i);
+        ECS_Vector** transform2d_component_vector = ECS_Vector_Get(registry->component_types, transform2d_component_type.id);
+        ECS_Vector** physics2d_component_vector = ECS_Vector_Get(registry->component_types, physics2d_component_type.id);
+
+        Transform2DComponent* entity_transform = ECS_Vector_Get(*transform2d_component_vector, entity->id);
+        Physics2DComponent* physics = ECS_Vector_Get(*physics2d_component_vector, entity->id);
+
+        entity_transform->x += physics->velocity_x;
+        entity_transform->y += physics->velocity_y;        
+    }
+}
 
 void Render2DSystem_Init() 
 {
@@ -94,6 +127,7 @@ int main()
     ECS_Registry_New(&registry);
 
     ECS_Registry_RegisterComponentType(registry, sizeof(Transform2DComponent), &transform2d_component_type);
+    ECS_Registry_RegisterComponentType(registry, sizeof(Physics2DComponent), &physics2d_component_type);
 
     Render2DSystem render2d_system_data = 
     {
@@ -108,13 +142,29 @@ int main()
 
     ECS_Registry_CreateSystem(registry, &render2d_system);
 
+    Physics2DSystem physics2d_system_data = 
+    {
+
+    };
+
+    ECS_System physics2d_system;
+    physics2d_system.required_entity_components_bitset = (1ULL << transform2d_component_type.id) | (1ULL << physics2d_component_type.id);
+    physics2d_system.Init = Physics2DSystem_Init;
+    physics2d_system.Update = Physics2DSystem_Update;
+    physics2d_system.data = (void*) &physics2d_system_data;
+
+    ECS_Registry_CreateSystem(registry, &physics2d_system);
+
     ECS_Entity player, enemy;
     ECS_Registry_CreateEntity(registry, &player);
     ECS_Registry_CreateEntity(registry, &enemy);
 
     Transform2DComponent player_transform = { 100.0f, 100.0f };
+    Physics2DComponent player_physics = { 2.0f, 0.0f };
     Transform2DComponent enemy_transform = { 250.0f, 120.0f };
     ECS_Registry_AddComponentToEntity(registry, transform2d_component_type, &player_transform, player);
+    ECS_Registry_AddComponentToEntity(registry, physics2d_component_type, &player_physics, player);
+
     ECS_Registry_AddComponentToEntity(registry, transform2d_component_type, &enemy_transform, enemy);
 
     ECS_Registry_PublishEntity(registry, player);
